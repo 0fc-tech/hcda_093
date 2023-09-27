ff9d331a25622b0613a38ed3a80820f9afcc56ab
quentin <quentinrouet@hotmail.Fr>
2023-09-27 10:01:34 +0200

    Demo
    Demo Mod 7

--- lib/demo_mod7.dart
+++ lib/demo_mod7.dart
@@ -30,28 +30,59 @@ class DemoFormWidget extends StatefulWidget {
 class _DemoFormWidgetState extends State<DemoFormWidget> {
   final textControllerEmail = TextEditingController();
   bool rememberMe = false;
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body: Form(
-          child: Column(
-            children: [
-              TextFormField(
-                  controller: textControllerEmail,
-                  decoration: InputDecoration(
-                      labelText: "E-mail",
-                      hintText: "john@kennedy.gov.com")
-              ),
-              Switch(value: rememberMe,onChanged: (isEnabled){
-                setState(() {
-                  rememberMe = !rememberMe;
-                });
-              }),
-              ElevatedButton(onPressed: (){}, child: Text("Valider"))
-          ],)
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                      controller: textControllerEmail,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                          labelText: "E-mail",
+                          hintText: "john@kennedy.gov.com"),
+                      validator: (valueEmail){
+                        if(valueEmail == null || valueEmail.isEmpty == true){
+                          return "Merci de compléter ce champ";
+                        }
+                        if(!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z.]{2,6}$").hasMatch(valueEmail)){
+                          return "Vérifier le format (ex: john@kennedy.gov)";
+                        }
+                        return null;
+                      },
+                  ),
+                  Switch(value: rememberMe,onChanged: (isEnabled){
+                    setState(() {
+                      rememberMe = !rememberMe;
+                    });
+                  }),
+                  ElevatedButton(
+                  onPressed: (){
+                    print(textControllerEmail.text);
+                    print(rememberMe);
+                    if(_formKey.currentState?.validate() == true){
+                      print("Formulaire correcte");
+
+                    }else{
+                      print("Formulaire incorrecte");
+                    }
+
+                  },
+                  child: Text("Valider"))
+              ],)
+          ),
+        ),
       ),
     );
   }
+
 }
 
 
